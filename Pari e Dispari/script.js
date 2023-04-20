7414bd2c851cc0f8cc2a8cd1b0240a4f872b3776
DanieleSavoia <danielesavoia44@gmail.com>
2023-04-20 18:13:29 +0200

    commit4

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -3,9 +3,9 @@
 let paroDisp = prompt("pari o dispari?")
 let numUno = prompt("inserisci un numero da 1 a 5")
 
+
 document.getElementById("sceltaUno").innerHTML = paroDisp;
 document.getElementById("sceltaDue").innerHTML = numUno;
-
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 
 function casuale() {
@@ -26,24 +26,20 @@ function somma() {
 
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
-   if (sommaNum % 2 ==0) {
+   if (sommaNum % 2 == 0) {
     document.getElementById("risFinale").innerHTML = "pari";
     
    }else {
     document.getElementById("risFinale").innerHTML = "dispari";
    }
+   return(sommaNum);
 }
-
 //Dichiariamo chi ha vinto.
 
 function vincitore() {
-    if (sommaNum == numDue) {
-        document.getElementById("vincitore").innerHTML = "il vincitore é l'utente";  
+    if (paroDisp === "sommaNum") {
+        document.getElementById("vinc").innerHTML = "il vincitore é l'utente!";  
     }else {
-        document.getElementById("vincitore").innerHTML = "il vincitore é il computer";
+        document.getElementById("vinc").innerHTML = "il vincitore é il computer!";
     }
-     
-
-
 }
-
